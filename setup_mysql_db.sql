7f296171356aa9456a1dd21db57dc26fddd8b896
root <adib.grouz@gmail.com>
2020-06-10 15:43:53 +0100

    .gitignore

--- setup_mysql_db.sql
+++ setup_mysql_db.sql
@@ -1,5 +1,7 @@
 DROP DATABASE IF EXISTS mynd;
-CREATE DATABASE IF NOT EXISTS mynd;
-CREATE USER IF NOT EXISTS 'mynd_user'@'localhost' IDENTIFIED BY 'mynd';
+CREATE DATABASE
+IF NOT EXISTS mynd;
+CREATE USER
+IF NOT EXISTS 'mynd_user'@'localhost' IDENTIFIED BY 'mynd';
 GRANT ALL PRIVILEGES ON `mynd`.* TO 'mynd_user'@'localhost';
 GRANT SELECT ON `performance_schema`.* TO 'mynd_user'@'localhost';
