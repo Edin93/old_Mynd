4b8a44ff194955e6eccb60a8fc182fdcccd94055
root <adib.grouz@gmail.com>
2020-06-14 15:39:55 +0100

    [add] logs routes

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -8,7 +8,7 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.users import *
 from api.v1.views.auth import *
-# from api.v1.views.logs import *
+from api.v1.views.logs import *
 # from api.v1.views.comments import *
 # from api.v1.views.posts import *
 from api.v1.views.topics import *
--- api/v1/views/logs.py
+++ api/v1/views/logs.py
@@ -5,16 +5,22 @@ from models import storage
 from api.v1.views import app_views
 from flask import abort, jsonify, make_response, request
 from flask_jwt import JWT, jwt_required, current_identity
+from api.v1.views.util.helpers import ClientError
+
 
 @app_views.route('/logs', methods=['GET'])
 @jwt_required()
-def post_comments():
+def logs():
     """Get all logs."""
-    pass
+    logs = storage.all(Log).values()
+    logs = [log.to_dict() for log in logs]
+    return jsonify({'status_code': 1, 'record': 'logs', 'logs': logs})
 
 
-@app_views.route('/logs/<string:username>', methods=['GET'])
+@app_views.route('/logs/activity', methods=['GET'])
 @jwt_required()
-def post_comments(username):
+def activity_logs():
     """Get all username logs."""
-    pass
+    logs = storage.all(Log).values()
+    logs = [log.to_dict() for log in logs if log.to_dict()['user_id'] == current_identity['id']]
+    return jsonify({'status_code': 1, 'user_id': current_identity['id'], 'logs': logs})
