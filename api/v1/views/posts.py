ce30c96e75fb607b330b34dc38a195ec1126aa85
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2020-06-14 15:39:03 +0100

    fix routes, add post likes api methods

--- api/v1/views/posts.py
+++ api/v1/views/posts.py
@@ -3,6 +3,7 @@
 from models.comment import Comment
 from models.topic import Topic
 from models.post import Post
+from models.post_like import PostLike
 from models import storage
 from api.v1.views import app_views
 from flask import abort, jsonify, make_response, request
@@ -159,21 +160,16 @@ def post_topic(username, post_id, topic_id):
     return {"status_code": 1, "info": "Topic deleted from this post"}
 
 
-@app_views.route('/user/<string:username>/post/<string:post_id>/comments', methods=['POST', 'GET'])
+@app_views.route('/post/<string:post_id>/comments', methods=['POST', 'GET'])
 @jwt_required()
-def post_comments(username, post_id):
+def post_comments(post_id):
     """Get the comments of a post, or adds a comment"""
-    user = storage.get_user_by_username(username)
-    is_me = current_identity['username'] == username
-    if not user:
-        return ClientError(404, 'User not found', 'Not Found')
     post = storage.get(Post, post_id)
     if not post:
         return ClientError(404, 'Post not found', 'Not Found')
-    if post not in user.posts:
-        return ClientError(404, 'Post not found for this user', 'Not Found')
+    user = storage.get(User, post.user_id)
     if request.method == 'GET':
-        d = {'Post owner': username, 'Post id': post_id, 'number of comments': len(post.comments), 'comments' = []}
+        d = {'Post owner': user.username, 'Post id': post_id, 'number of comments': len(post.comments), 'comments' = []}
         for comment in post.comments:
             dc = {'comment id': comment.id,
                   'written by': storage.get(User, comment.user_id).username,
@@ -182,6 +178,7 @@ def post_comments(username, post_id):
         return jsonify(d)
     if not request.get_json():
         return ClientError(400, 'Not a JSON', 'Invalid')
+    comment = Comment()
     comment.user_id = current_identity['id']
     comment.post_id = post_id
     if 'text' in request.get_json():
@@ -194,23 +191,18 @@ def post_comments(username, post_id):
     return {"status_code": 1, "info": "Created"}
 
 
-@app_views.route('/user/<string:username>/post/<string:post_id>/comment/<string:comment_id>', methods=['DELETE', 'PUT'])
+@app_views.route('/post/<string:post_id>/comment/<string:comment_id>', methods=['DELETE', 'PUT'])
 @jwt_required()
-def edit_post_comment(username, post_id, comment_id):
+def edit_post_comment(post_id, comment_id):
     """Modify or delete a post comment."""
-    user = storage.get_user_by_username(username)
-    is_me = current_identity['username'] == username
-    if not user:
-        return ClientError(404, 'User not found', 'Not Found')
     post = storage.get(Post, post_id)
     if not post:
         return ClientError(404, 'Post not found', 'Not Found')
-    if post not in user.posts:
-        return ClientError(404, 'Post not found for this user', 'Not Found')
     comment = storage.get(Comment, comment_id)
     if not comment or comment not in post.comments:
         return ClientError(404, 'Comment Not Found', 'Not Found')
-    if comment.user_id != current_identity['id'] and comment.user_id != user.id:
+    user = storage.get(User, post.user_id)
+    if comment.user_id != current_identity['id'] and current_identity['id'] != user.id:
         return ClientError(401, 'Access denied', 'Unauthorized')
     if request.method == 'DELETE':
         storage.delete(comment)
@@ -232,15 +224,44 @@ def edit_post_comment(username, post_id, comment_id):
 
 @app_views.route('/post/<string:post_id>/like', methods=['POST'])
 @jwt_required()
-def like_post():
-    pass
+def like_post(post_id):
+    """ likes a post """
+    post = storage.get(Post, post_id)
+    if not post:
+        return ClientError(404, 'Post not found', 'Not Found')
+    for like in post.likes:
+        if like.user_id == current_identity['id']:
+            return {"status_code": 1, "info": "You already like it!"}
+    new_like = PostLike()
+    new_like.user_id = current_identity['id']
+    new_like.post_id = post_id
+    new_like.save()
+    return {"status_code": 1, "info": "Post successfully liked"}
+
 
 @app_views.route('/post/<string:post_id>/unlike', methods=['POST'])
 @jwt_required()
-def unlike_post():
-    pass
+def unlike_post(post_id):
+    """ unlike a post """
+    post = storage.get(Post, post_id)
+    if not post:
+        return ClientError(404, 'Post not found', 'Not Found')
+    for like in post.likes:
+        if like.user_id == current_identity['id']:
+            storage.delete(like)
+            storage.save()
+            return {"status_code": 1, "info": "Post successfully unliked"}
+    return {"status_code": 1, "info": "You already don't like it"}
+
 
 @app_views.route('/post/<string:post_id>/likes/', methods=['GET'])
 @jwt_required()
 def post_likers(post_id):
-    pass
+    """ returns likers of a post"""
+    post = storage.get(Post, post_id)
+    if not post:
+        return ClientError(404, 'Post not found', 'Not Found')
+    d = {'post id': post.id, 'number of likes': len(post.likes), 'liked by': []}
+    for like in post.likes:
+        d['liked by'].append(storage.get(User, like.user_id).username)
+    return jsonify(d)
