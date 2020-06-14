a2ed32b0c9babb7aeb2cfb7e8916570e8ffbd325
edin93 <il_matador@hotmail.fr>
2020-06-14 12:50:17 +0100

    [add] landing page

---
+++ mynd_landing_page/js/magnificpopup.int.js
@@ -0,0 +1,9 @@
+    //  VIDEO 
+$('.video-play-icon').magnificPopup({
+    disableOn: 700,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+    fixedContentPos: false
+});
