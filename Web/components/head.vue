1f90fa844c8ed3d817478b2101908dba04922b2f
root <adib.grouz@gmail.com>
2020-06-15 23:56:42 +0100

    [init] layout, post and user pages

---
+++ Web/components/head.vue
@@ -0,0 +1,131 @@
+<template>
+  <div>
+    <sui-menu fixed>
+      <sui-container>
+        <a is="sui-menu-item" class="header">
+          <img src="../assets/images/logo.png" class="logo" />
+          Mynd
+        </a>
+      </sui-container>
+      <sui-menu-menu position="right">
+        <a @click.native="toggleJoin" is="sui-menu-item" class="header">
+          Join
+        </a>
+        <a @click.native="toggleLogin" is="sui-menu-item" class="header">
+          Login
+        </a>
+        <sui-menu-item>
+          <sui-input transparent icon="search" placeholder="Search..." />
+        </sui-menu-item>
+      </sui-menu-menu>
+    </sui-menu>
+
+    <sui-modal size="mini" v-model="login">
+      <sui-modal-header>Login</sui-modal-header>
+
+      <sui-form>
+        <sui-segment>
+          <sui-form-field>
+            <sui-input
+              type="email"
+              placeholder="E-mail address"
+              icon="user"
+              icon-position="left"
+            />
+          </sui-form-field>
+          <sui-form-field>
+            <sui-input
+              type="password"
+              placeholder="Password"
+              icon="lock"
+              icon-position="left"
+            />
+          </sui-form-field>
+          <sui-button size="large" fluid>Login</sui-button>
+        </sui-segment>
+      </sui-form>
+
+      <sui-message>New to Mynd? <a @click="toggle">Sign Up</a> </sui-message>
+    </sui-modal>
+    <sui-modal size="mini" v-model="join">
+      <sui-modal-header>Join Mynd</sui-modal-header>
+
+      <sui-form>
+        <sui-segment>
+          <sui-form-field>
+            <sui-input type="email" placeholder="E-mail address" />
+          </sui-form-field>
+          <sui-form-field>
+            <sui-input type="username" placeholder="Username" />
+          </sui-form-field>
+          <sui-form-field>
+            <sui-input type="password" placeholder="Password" />
+          </sui-form-field>
+          <sui-form-field>
+            <sui-dropdown
+              placeholder="Gender"
+              selection
+              :options="options"
+              v-model="current"
+            />
+          </sui-form-field>
+
+          <sui-button size="large" fluid>Join</sui-button>
+        </sui-segment>
+      </sui-form>
+      <sui-message>Already on Mynd? <a @click="toggle">Login</a></sui-message>
+    </sui-modal>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Head",
+  data() {
+    return {
+      join: false,
+      login: false,
+      current: null,
+      options: [
+        {
+          text: "Male",
+          value: 1
+        },
+        {
+          text: "Female",
+          value: 2
+        }
+      ]
+    };
+  },
+  methods: {
+    toggleJoin() {
+      this.join = !this.join;
+    },
+    toggleLogin() {
+      this.login = !this.login;
+    },
+    toggle() {
+      this.join = !this.join;
+      this.login = !this.login;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.message {
+  margin-top: 0;
+}
+.button {
+  color: aliceblue;
+  background: #5a4fdc;
+}
+.button:hover {
+  color: antiquewhite;
+  background: #6e64d8;
+}
+a {
+  color: #5a4fdc;
+}
+</style>
---
+++ Web/pages/post/_id.vue
@@ -0,0 +1,144 @@
+<template>
+  <div class="main">
+    <sui-grid centered :columns="3">
+      <sui-grid-column>
+        <!-- <sui-segment> -->
+        <sui-image
+          src="https://semantic-ui.com/images/avatar2/large/kristy.png"
+          fluid
+        />
+
+        <sui-comment-group>
+          <h3 is="sui-header" dividing>Comments</h3>
+
+          <sui-comment>
+            <sui-comment-avatar
+              src="https://semantic-ui-vue.github.io/static/images/avatar/small/matt.jpg"
+            />
+            <sui-comment-content>
+              <a is="sui-comment-author">Matt</a>
+              <sui-comment-metadata>
+                <div>Today at 5:42PM</div>
+              </sui-comment-metadata>
+              <sui-comment-text>How artistic!</sui-comment-text>
+              <sui-comment-actions>
+                <sui-comment-action>Reply</sui-comment-action>
+              </sui-comment-actions>
+            </sui-comment-content>
+          </sui-comment>
+
+          <sui-comment>
+            <sui-comment-avatar
+              src="https://semantic-ui-vue.github.io/static/images/avatar/small/matt.jpg"
+            />
+            <sui-comment-content>
+              <a is="sui-comment-author">Elliot Fu</a>
+              <sui-comment-metadata>
+                <div>Yesterday at 12:30AM</div>
+              </sui-comment-metadata>
+              <sui-comment-text>
+                <p>
+                  This has been very useful for my research. Thanks as well!
+                </p>
+              </sui-comment-text>
+              <sui-comment-actions>
+                <sui-comment-action>Reply</sui-comment-action>
+              </sui-comment-actions>
+            </sui-comment-content>
+          </sui-comment>
+
+          <sui-comment>
+            <sui-comment-avatar
+              src="https://semantic-ui-vue.github.io/static/images/avatar/small/matt.jpg"
+            />
+            <sui-comment-content>
+              <a is="sui-comment-author">Joe Henderson</a>
+              <sui-comment-metadata>
+                <div>5 days ago</div>
+              </sui-comment-metadata>
+              <sui-comment-text>
+                Dude, this is awesome. Thanks so much
+              </sui-comment-text>
+              <sui-comment-actions>
+                <sui-comment-action>Reply</sui-comment-action>
+              </sui-comment-actions>
+            </sui-comment-content>
+          </sui-comment>
+
+          <sui-form reply>
+            <textarea rows="2" class="pri"></textarea>
+
+            <sui-button
+              content="Add Reply"
+              label-position="left"
+              icon="edit"
+              primary
+              class="pri"
+            />
+          </sui-form>
+        </sui-comment-group>
+
+        <sui-rail position="right">
+          <sui-segment>
+            <div>
+              <sui-image
+                src="https://semantic-ui-vue.github.io/static/images/avatar/small/matt.jpg"
+                avatar
+              />
+              <span>Username</span>
+            </div>
+            <div text-align="left" class="details">
+              <b>Description</b>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Sapiente, suscipit?
+              </p>
+            </div>
+            <div class="tags">
+              <a is="sui-label" tag>
+                Chill
+              </a>
+              <a is="sui-label" tag>
+                Travel
+              </a>
+              <a is="sui-label" tag>
+                Nature
+              </a>
+            </div>
+            <div class="likes">
+              <sui-icon
+                slot="trigger"
+                name="heart"
+                color="red"
+                size="large"
+              /> 2k
+            </div>
+          </sui-segment>
+        </sui-rail>
+        <!-- </sui-segment> -->
+      </sui-grid-column>
+    </sui-grid>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+<style scoped>
+.main {
+  margin-top: 7em;
+}
+
+.pri {
+  margin-top: 1em;
+}
+.details {
+  margin: 1.5em 0;
+}
+.tag {
+  margin: 5px;
+}
+.likes {
+    margin-top: 1em;
+}
+</style>
